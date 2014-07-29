38a1daf4625b1b785efabc2517e2030d3a52cdef
Michael Dong <michael.dong@rackspace.com>
2014-07-29 10:40:37 -0500

    models created

--- shuttle/models.py
+++ shuttle/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class WifiInfo(models.Model):
+	router_name = models.CharField(max_length=128)
+	password = models.CharField(max_length=128)
+class Location(models.Model):
+	location = models.CharField(max_length=128)
+class Shuttle(models.Model):
+	time = models.DateField()
+	capacity = models.IntegerField()
+	num_signedup = models.IntegerField()
+	num_checkedin = models.IntegerField()
+	num_on_waitlist = models.IntegerField()
+	departure_location = models.ForeignKey(Location, related_name='departure_location')
+	arrival_location = models.ForeignKey(Location, related_name='arrival_location')
+class Racker(models.Model):
+	username = models.CharField(max_length=128)
+	name = models.CharField(max_length=128)
+	phone = models.CharField(max_length=64)
+	checked_in_morning = models.ForeignKey(Shuttle, null=True, related_name='checked_in_morning')
+	waiting_for_morning = models.ForeignKey(Shuttle, null=True, related_name='waiting_for_morning')
+	signed_in_morning = models.ForeignKey(Shuttle, null=True, related_name='signed_in_morning')
+	signed_in_afternoon = models.ForeignKey(Shuttle, null=True, related_name='signed_in_afternoon')
+	checked_in_afternoon = models.ForeignKey(Shuttle, null=True, related_name='checked_in_afternoon')
+	waiting_for_afternoon = models.ForeignKey(Shuttle, null=True, related_name='waiting_for_afternoon')
+
